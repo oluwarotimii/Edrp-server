550d94958cd9c8d5822245fd4ce8912add5da9a3
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-06-14 12:13:45 +0100

    fix: update deployment script with database migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from logging.config import fileConfig
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
